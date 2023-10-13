2c3b6ded65a1c214af9254e825793e57c0a49b2c
Akhil Balakrishnan <akhil.b@litmus7.com>
2023-10-13 14:27:23 +0530

    Initial Commit

---
+++ KmmiOs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KmmiOs
+//
+//  Created by Akhil.b on 13/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KmmiOs/KmmiOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KmmiOsApp.swift
+//  KmmiOs
+//
+//  Created by Akhil.b on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct KmmiOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
